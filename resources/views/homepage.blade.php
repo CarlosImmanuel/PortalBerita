6afb0255e496169e8ca179cac39f0d31c15e783a
CarlosImmanuel <carlosimmanuel1208@gmail.com>
2025-06-02 21:27:50 +0700

    Test

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -92,7 +92,7 @@
     <!-- Footer -->
     @include('layouts.footer')
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="{{ asset('js/search.js') }}"></script>
+  <script src="{{ asset('js/search.js') }}"></script> {{-- Menambahkan Script JS --}}
 
 
 </body>
